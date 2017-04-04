1c9f9d7d0e7a961370f4a18cd21f3ae541d30b12
Andrius Mudinas <keistokas@gmail.com>
2017-04-04 12:19:30 +0100

    Adding acceptance/integration tests

---
+++ Code/Wikiled.MachineLearning.Svm.Tests/Acceptance/SvmClientTests.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using NUnit.Framework;
+using Wikiled.Arff.Persistence;
+using Wikiled.MachineLearning.Svm.Clients;
+using Wikiled.MachineLearning.Svm.Logic;
+
+namespace Wikiled.MachineLearning.Svm.Tests.Acceptance
+{
+    [TestFixture]
+    public class SvmClientTests
+    {
+
+        [TestCase("data_b.arff", "classify.dat", PositivityType.Positive)]
+        [TestCase("testing_data_1.arff", "label.dat", "One")]
+        public void Classify(string arff, string modelName, object result)
+        {
+            var file = Path.Combine(TestContext.CurrentContext.TestDirectory, "data", arff);
+            var dataSet = result.GetType() == typeof(PositivityType)
+                              ? ArffDataSet.Load<PositivityType>(file)
+                              : ArffDataSet.LoadSimple(file);
+            file = Path.Combine(TestContext.CurrentContext.TestDirectory, "data", modelName);
+            var model = Model.Read(file);
+            var client = new SvmTestClient(dataSet, model);
+
+            var dataHolder = client.CreateTestDataset();
+            var review = dataHolder.AddDocument();
+            review.AddRecord("Good").Value = 2;
+            review.AddRecord("Bad").Value = 1;
+            client.Classify(dataHolder);
+            Assert.AreEqual(result, review.Class.Value);
+        }
+    }
+}
---
+++ Code/Wikiled.MachineLearning.Svm.Tests/Acceptance/TestSvm.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using NLog;
+using NUnit.Framework;
+using Wikiled.Arff.Persistence;
+using Wikiled.MachineLearning.Svm.Logic;
+using Wikiled.MachineLearning.Svm.Parameters;
+
+namespace Wikiled.MachineLearning.Svm.Tests.Acceptance
+{
+    [TestFixture]
+    public class TestSvm
+    {
+        private Parameter parameters;
+
+        private TrainingModel model;
+
+        private static readonly Logger log = LogManager.GetCurrentClassLogger();
+
+        [SetUp]
+        public void Setup()
+        {
+            var header = TrainingHeader.CreateDefault();
+            parameters = new Parameter();
+            parameters.KernelType = header.Kernel;
+            parameters.CacheSize = 200;
+            parameters.SvmType = header.SvmType;
+            parameters.Probability = false;
+            model = new TrainingModel();
+        }
+
+        [Test]
+        public void Test()
+        {
+            log.Info("Test");
+            var problem = LoadData();
+            parameters.Weights = WeightCalculation.GetWeights(problem.Y);
+
+            double test = model.PerformCrossValidation(problem, parameters, 5);
+            Assert.AreEqual(0.88, Math.Round(test, 2));
+        }
+
+        [Test]
+        public async Task Cancel()
+        {
+            log.Info("Cancel");
+            var problem = LoadData();
+            parameters.Weights = WeightCalculation.GetWeights(problem.Y);
+
+            CancellationTokenSource source = new CancellationTokenSource();
+            parameters.Token = source.Token;
+            var task = Task.Run(() => model.PerformCrossValidation(problem, parameters, 5));
+            await Task.Delay(5000);
+            source.Cancel();
+            await Task.Delay(5000);
+            Assert.IsTrue(task.IsFaulted);
+        }
+
+        private Problem LoadData()
+        {
+            var file = Path.Combine(TestContext.CurrentContext.TestDirectory, @".\Data\data.arff");
+            var arff = ArffDataSet.Load<PositivityType>(file);
+            return arff.GetProblem();
+        }
+    }
+}
